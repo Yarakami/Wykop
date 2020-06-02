bba59e39216a48c06d21989aafa457a6aa76c94a
Yarakami <66177567+Yarakami@users.noreply.github.com>
2020-06-02 02:52:57 +0200

    Add plugin settings

--- Przypominacz.user.js
+++ Przypominacz.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Przypominacz
 // @namespace    przypominacz
-// @version      0.1
+// @version      0.2
 // @description  Przypomina tobie o wpisie na mikro
 // @author       Yarakami
 // @match        https://www.wykop.pl/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
@@ -12,12 +13,78 @@
 (function() {
     'use strict';
 
-    var myStorage = window.localStorage;
-    var currentUrl = window.location.href;
+    let myStorage = window.localStorage;
+    let currentUrl = window.location.href;
 
-    var navBar = null;
-    var ul = null
-    var posts = null;
+    let navBar = null;
+    let ul = null
+    let posts = null;
+
+    let nav = document.getElementsByClassName("m-hide")[0];
+
+    let nI = document.createElement("i");
+    nI.classList.add("fa");
+    nI.classList.add("fa-info-circle");
+
+    let nLink = document.createElement("a");
+    nLink.classList.add("dropdown-show");
+    CreateCounter();
+    setInterval(CreateCounter, 3000);
+
+
+
+    nLink.href = "";
+    nLink.appendChild(nI);
+
+
+
+    let nLi = document.createElement("li");
+    nLi.addEventListener("click", Redirect);
+    nLi.appendChild(nLink);
+    nLi.classList.add("notification");
+    nav.parentNode.insertBefore(nLi, nav.nextSibling);
+
+    function CreateCounter()
+    {
+        let postsCount = GetSavedPosts();
+        let notificationsCount = GetNotifications(postsCount);
+        if(notificationsCount >0)
+        {
+            let counter = document.getElementsByClassName("counter")[0];
+            if(counter)
+            {
+                counter.innerHTML = notificationsCount;
+            }
+            else
+            {
+                let nB = document.createElement("b");
+                nB.classList.add("counter");
+                nB.innerHTML = notificationsCount;
+                nLink.appendChild(nB);
+            }
+        }
+        else
+        {
+            let counter = document.getElementsByClassName("counter")[0];
+            if(counter) counter.remove();
+        }
+    }
+
+    function GetNotifications(array)
+    {
+        let count = 0;
+        let date = moment();
+        array.forEach(element => {
+            if(IsOldDate(element.date))count ++;
+        })
+
+        return count;
+    }
+
+    function Redirect()
+    {
+        window.location.href = "https://www.wykop.pl/ustawienia/przypominacz/";
+    }
 
     if(currentUrl.includes("ustawienia"))
     {
@@ -28,16 +95,16 @@
             if(element.childElementCount >5)
             {
                 //Create span with text
-                var nSpan = document.createElement("span");
+                let nSpan = document.createElement("span");
                 nSpan.innerHTML = "Przypominacz";
 
                 //Create link
-                var nHref = document.createElement('a');
+                let nHref = document.createElement('a');
                 nHref.href = "https://www.wykop.pl/ustawienia/przypominacz/";
                 nHref.appendChild(nSpan); //Append span with text to link
 
                 //Create li element
-                var nLi = document.createElement("LI");
+                let nLi = document.createElement("LI");
                 nLi.appendChild(nHref); //Append
 
                 ul = element;
@@ -53,7 +120,7 @@
     {
         //Page preparation - remove unwanted elements
         document.getElementsByClassName("rbl-block space ")[0].remove();
-        var settingsForm = document.getElementsByClassName("settings")[0];
+        let settingsForm = document.getElementsByClassName("settings")[0];
         settingsForm.innerHTML = '';
 
         //
@@ -61,27 +128,49 @@
         {
             if(element.firstChild)
             {
-                var child = element.firstChild;
+                let child = element.firstChild;
                 if(child.innerText === "Ustawienia") element.classList.remove("active");
                 if(child.innerText === "Przypominacz") element.classList.add("active");
             }
         });
 
-        var nFieldset0 = document.createElement("fieldset");
-        var nHeadline0 = document.createElement("h4");
+        let nFieldset0 = document.createElement("fieldset");
+        let nHeadline0 = document.createElement("h4");
         nHeadline0.innerHTML = "Ustawienia przypominacza";
 
-        var nDiv0 = document.createElement("div");
-        nDiv0.classList.add("space");
+        let nForm0 = document.createElement("form");
+        nForm0.classList.add("width-two-third");
+
+        let nForm0Label = document.createElement("label");
+        nForm0Label.innerHTML = "Czas przypomnienia (Minuty)";
+        let nForm0Input = document.createElement("input");
+        nForm0Input.type = "text";
+        nForm0Input.classList.add("interval");
+        let intTime = myStorage.getItem("interval");
+        if(intTime) nForm0Input.value = intTime;
+        nForm0.appendChild(nForm0Label);
+        nForm0.appendChild(nForm0Input);
+
+        let nFieldset = document.createElement("fieldset");
+        nFieldset.classList.add("row");
+        nFieldset.classList.add("button");
+        nFieldset.setAttribute("style", "background: transparent; border: transparent;");
+
+        let nFieldsetButton = document.createElement("button");
+        //nFieldsetButton.classList.add("submit");
+        nFieldsetButton.innerHTML = "Zapisz";
+        nFieldsetButton.addEventListener("click", ChangeInterval);
+        nFieldset.appendChild(nFieldsetButton);
 
         nFieldset0.appendChild(nHeadline0);
-        nFieldset0.appendChild(nDiv0);
+        nFieldset0.appendChild(nForm0);
+        nFieldset0.appendChild(nFieldset);
 
-        var nFieldset1 = document.createElement("fieldset");
-        var nHeadline1 = document.createElement("h4");
+        let nFieldset1 = document.createElement("fieldset");
+        let nHeadline1 = document.createElement("h4");
         nHeadline1.innerHTML = "Lista wpisów";
 
-        var nDiv1 = document.createElement("div");
+        let nDiv1 = document.createElement("div");
         nDiv1.classList.add("space");
         nDiv1.classList.add("nPosts");
 
@@ -97,24 +186,24 @@
 
     function CreatePostsList()
     {
-        var postsParent = document.getElementsByClassName("nPosts")[0];
-        var postsUl = document.getElementsByClassName("nPostsData")[0];
+        let postsParent = document.getElementsByClassName("nPosts")[0];
+        let postsUl = document.getElementsByClassName("nPostsData")[0];
         if(postsUl) postsUl.remove();
-        var posts = GetSavedPosts();
+        let posts = GetSavedPosts();
         if(posts.length ==0)
         {
-            var info0 = document.createElement("div");
+            let info0 = document.createElement("div");
             info0.classList.add("nPostsData");
-            var infoParagraph0 = document.createElement("p");
+            let infoParagraph0 = document.createElement("p");
             infoParagraph0.innerHTML = "Brak zapisanych wpisów ( ͡° ʖ̯ ͡°)";
             info0.appendChild(infoParagraph0);
             postsParent.appendChild(info0);
         }
         else
         {
-            var info1 = document.createElement("div");
+            let info1 = document.createElement("div");
             info1.classList.add("nPostsData");
-            var infoParagraph1 = document.createElement("p");
+            let infoParagraph1 = document.createElement("p");
             infoParagraph1.innerHTML = "Liczba zapisanych wpisów: "+posts.length+"  ᕦ(òóˇ)ᕤ";
             info1.appendChild(infoParagraph1);
             postsParent.appendChild(info1);
@@ -130,15 +219,25 @@
 
     function CheckButton(postId, buttonAction)
     {
-        var activeElement = document.getElementsByClassName("vC")[0];
-        var isInHistory = null;
+        let activeElement = document.getElementsByClassName("vC")[0];
+        let isInHistory = null;
 
         if(!myStorage.getItem(postId))
         {
             isInHistory = false;
             if(buttonAction)
             {
-                 myStorage.setItem(postId , "asd");
+                let date = moment();
+
+                console.log(moment(date).toISOString());
+                let inter = myStorage.getItem("interval");
+                let timeToAdd = null;
+                if(inter) timeToAdd = inter;
+                else inter = 15;
+
+                let added = moment(date).add(inter, 'minutes').toISOString();
+                console.log(added);
+                myStorage.setItem(postId , added);
                 isInHistory = true;
             }
 
@@ -152,8 +251,8 @@
             }
         }
 
-        var nI = null;
-        var nA = null;
+        let nI = null;
+        let nA = null;
 
         if(!isInHistory)
         {
@@ -183,28 +282,35 @@
         }
     }
 
+    function IsOldDate(dateToCheck)
+    {
+        let dateNow = moment();
+        dateToCheck = moment(dateToCheck);
+        if(dateNow>= dateToCheck) return true;
+        else return false;
+    }
+
 
     function OnClick()
     {
-        var asd = GetCurrentPostId();
         CheckButton(GetCurrentPostId(), true);
     };
 
 
     function GetCurrentPostId()
     {
-        var pth = window.location.pathname.replace('/wpis/','');
-        var n = pth.indexOf('/');
+        let pth = window.location.pathname.replace('/wpis/','');
+        let n = pth.indexOf('/');
         return pth.substring(0, n != -1 ? n : pth.length);;
     }
 
     function GetSavedPosts()
     {
-        var savedPosts = [];
-        for (var key in myStorage){
+        let savedPosts = [];
+        for (let key in myStorage){
             if(!isNaN(key))
             {
-                var sp =
+                let sp =
                 {
                     id: key,
                     date: myStorage.getItem(key)
@@ -213,6 +319,29 @@
             }
         }
         return savedPosts;
+    }
+
+    function ChangeInterval()
+    {
+        let intervalInput = document.getElementsByClassName("interval")[0];
+        if(intervalInput)
+        {
+            if(isNaN(intervalInput.value))
+            {
+                alert("Podaj poprawną liczbę");
+            }
+            else
+            {
+                let l = Math.round(intervalInput.value);
+                if(l <=5)alert("Liczba jest za mała");
+                else if(l >60)alert("Liczba jest za duża");
+                else
+                {
+                    myStorage.setItem("interval" , l);
+                }
+            }
+            //
+        }
     }
 
 
